ebe9b78987c1583c1f703140020bbe9f9b4a65dd
Donte5405 <donte5405@outlook.com>
2024-05-04 03:12:55 +0700

    Fix wrong implementation of the translator

--- gd/translator.gd
+++ gd/translator.gd
@@ -1,22 +1,25 @@
 class_name Tr
 
 
+#GODOG_PRIVATE: _key, _json, _file, _str, _path
+
+
 const TR_QUOTE = "_*_*_*_"
 
 
-static func dsk(str: String) -> String:
-    if TR_QUOTE in str:
+static func dsk(_key: String) -> String:
+    if TR_QUOTE in _key:
         # Debug translation, quite slow.
-        var _json: Dictionary = JSON.parse("{" + "".join(str.split(TR_QUOTE)) + "}").result
+        var _json: Dictionary = JSON.parse("{" + "".join(_key.split(TR_QUOTE)) + "}").result
         if _json.has(OS.get_locale_language()):
             return _json[OS.get_locale_language()]
-        return _str
+        return _key
     # Production translation.
     var _file := File.new()
-    var _str: String = OS.get_locale_language() + "_" + str
-    var path: String = "res://tr/" + _str.md5_text() + ".txt"
+    var _str: String = OS.get_locale_language() + "_" + _key
+    var _path: String = "res://tr/" + _str.md5_text() + ".txt"
     if not _file.file_exists(_path):
-        return str
-    _file.open()
+        return _key
+    _file.open(_path, File.READ)
     _str = _file.get_as_text()
     return _str
