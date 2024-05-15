99a341be73f5b02e5e699660fe240aa214b6f91e
Donte5405 <donte5405@outlook.com>
2024-05-16 00:42:04 +0700

    Always ignore files started with dot

--- src/lib/file.list.mjs
+++ src/lib/file.list.mjs
@@ -6,20 +6,17 @@ import * as Fs from "fs";
 /**
  * Search for all files in the directory.
  * @param {string} dir Directory location.
- * @param {boolean} ignoreFilesWithPrefixes Ignore files with '.' or '_' prefixes.
  * @param {string[]} [files] List of previous files (blank if not specified).
  */
-export function fileList(dir, ignoreFilesWithPrefixes = true, files = []) {
+export function fileList(dir, files = []) {
     Fs.readdirSync(dir).forEach(file => {
-        if (ignoreFilesWithPrefixes) {
-            switch (file[0]) {
-                case ".":
-                    return;
-            }
+        switch (file[0]) {
+            case ".":
+                return;
         }
         const absolute = Path.join(dir, file);
         if (Fs.statSync(absolute).isDirectory()) {
-            fileList(absolute, ignoreFilesWithPrefixes, files);
+            fileList(absolute, files);
             return;
         }
         files.push(absolute);
@@ -31,20 +28,17 @@ export function fileList(dir, ignoreFilesWithPrefixes = true, files = []) {
 /**
  * Search for all directories in the directory.
  * @param {string} dir Directory location.
- * @param {boolean} ignoreDirsWithPrefixes Ignore directories with '.' or '_' prefixes.
  * @param {string[]} [dirs] List of previous directories (blank if not specified).
  */
-export function dirList(dir, ignoreDirsWithPrefixes = true, dirs = []) {
+export function dirList(dir, dirs = []) {
     Fs.readdirSync(dir).forEach(file => {
-        if (ignoreDirsWithPrefixes) {
-            switch (file[0]) {
-                case ".": case "_":
-                    return;
-            }
+        switch (file[0]) {
+            case ".":
+                return;
         }
         const absolute = Path.join(dir, file);
         if (Fs.statSync(absolute).isDirectory()) {
-            dirList(absolute, ignoreDirsWithPrefixes, dirs);
+            dirList(absolute, dirs);
             dirs.push(absolute);
             return;
         }
