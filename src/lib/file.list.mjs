96e866ade708df5366f070ae7d9a0b811c256c9b
Donte5405 <donte5405@outlook.com>
2024-05-16 02:02:06 +0700

    Add a feature to ignore field names in the file listing function

--- src/lib/file.list.mjs
+++ src/lib/file.list.mjs
@@ -6,10 +6,12 @@ import * as Fs from "fs";
 /**
  * Search for all files in the directory.
  * @param {string} dir Directory location.
+ * @param {string[]} ignoreNames List of files/directories to be ignored.
  * @param {string[]} [files] List of previous files (blank if not specified).
  */
-export function fileList(dir, files = []) {
+export function fileList(dir, ignoreNames = [], files = []) {
     Fs.readdirSync(dir).forEach(file => {
+        if (ignoreNames.includes(file)) return;
         switch (file[0]) {
             case ".":
                 return;
@@ -28,10 +30,12 @@ export function fileList(dir, files = []) {
 /**
  * Search for all directories in the directory.
  * @param {string} dir Directory location.
+ * @param {string[]} ignoreNames List of files/directories to be ignored.
  * @param {string[]} [dirs] List of previous directories (blank if not specified).
  */
-export function dirList(dir, dirs = []) {
+export function dirList(dir, ignoreNames = [], dirs = []) {
     Fs.readdirSync(dir).forEach(file => {
+        if (ignoreNames.includes(file)) return;
         switch (file[0]) {
             case ".":
                 return;
