e24a2d13b80c57684313998ed00395c4d2b1e34f
Donte5405 <donte5405@outlook.com>
2024-05-16 04:32:26 +0700

    Improve docs of the file list functions

--- src/lib/file.list.mjs
+++ src/lib/file.list.mjs
@@ -6,19 +6,19 @@ import * as Fs from "fs";
 /**
  * Search for all files in the directory.
  * @param {string} dir Directory location.
- * @param {string[]} ignoreNames List of files/directories to be ignored.
+ * @param {string[]} ignoredFiles List of files/directories to be ignored.
  * @param {string[]} [files] List of previous files (blank if not specified).
  */
-export function fileList(dir, ignoreNames = [], files = []) {
+export function fileList(dir, ignoredFiles = [], files = []) {
     Fs.readdirSync(dir).forEach(file => {
-        if (ignoreNames.includes(file)) return;
+        if (ignoredFiles.includes(file)) return;
         switch (file[0]) {
             case ".":
                 return;
         }
         const absolute = Path.join(dir, file);
         if (Fs.statSync(absolute).isDirectory()) {
-            fileList(absolute, ignoreNames, files);
+            fileList(absolute, ignoredFiles, files);
             return;
         }
         files.push(absolute);
@@ -30,25 +30,25 @@ export function fileList(dir, ignoreNames = [], files = []) {
 /**
  * Search for all directories in the directory.
  * @param {string} dir Directory location.
- * @param {string[]} ignoreNames List of files/directories to be ignored.
+ * @param {string[]} excludeDirsWithFiles List of files/directories that's an indicator to disregard the entire directory.
  * @param {string[]} [dirs] List of previous directories (blank if not specified).
  */
-export function dirList(dir, ignoreNames = [ ".gdignore" ], dirs = []) {
-    for (const ignoreName of ignoreNames) {
-        const iAbsolute = Path.join(dir, ignoreName);
+export function dirList(dir, excludeDirsWithFiles = [ ".gdignore" ], dirs = []) {
+    for (const indicatorFile of excludeDirsWithFiles) {
+        const iAbsolute = Path.join(dir, indicatorFile);
         if (Fs.statSync(iAbsolute).isFile()) {
             return dirs;
         }
     }
     Fs.readdirSync(dir).forEach(file => {
-        if (ignoreNames.includes(file)) return;
+        if (excludeDirsWithFiles.includes(file)) return;
         switch (file[0]) {
             case ".":
                 return;
         }
         const absolute = Path.join(dir, file);
         if (Fs.statSync(absolute).isDirectory()) {
-            dirList(absolute, ignoreNames, dirs);
+            dirList(absolute, excludeDirsWithFiles, dirs);
             dirs.push(absolute);
             return;
         }
