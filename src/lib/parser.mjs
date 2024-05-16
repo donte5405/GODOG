5cc4716eb4fcd941168327c2b0e3022bbbd5da02
Donte5405 <donte5405@outlook.com>
2024-05-17 03:11:13 +0700

    Docs improvement

--- src/lib/parser.mjs
+++ src/lib/parser.mjs
@@ -317,7 +317,7 @@ export class GDParser {
                                 throw new Error(directFormatStringProhibitedErr(this.fileName));
                             }
                         }
-                    } else if (looksLikeStringPath(str)) { // Ignore string formatted string paths.
+                    } else if (looksLikeStringPath(str)) { // Ignore string formatted string-path-like because of the false-positive probability.
                         str = this.parse(str, "path");
                     }
                 }
